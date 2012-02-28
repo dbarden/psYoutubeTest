5204890f427ddd97738794237a3b2e664e6bd8b1
Daniel Barden <daniel.barden@gmail.com>
2012-02-27 21:31:04 -0300

    Creates a MPMoviePlayerViewController with a mp4 link

--- psYoutubeTest/ViewController.m
+++ psYoutubeTest/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "PSYouTubeExtractor.h"
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface ViewController ()
+@interface ViewController () {
+	PSYouTubeExtractor *_extractor;
+}
 
 @end
 
@@ -31,4 +35,35 @@
 	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	NSString *identifier = [NSString stringWithFormat:@"reuseId"];
+	
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+	
+	if (cell == nil) {
+		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+	}
+	cell.textLabel.text = @"test";
+	
+	return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	NSURL *youTubeURL = [NSURL URLWithString:@"http://www.youtube.com/watch?v=Vo0Cazxj_yc"];
+	_extractor = [PSYouTubeExtractor extractorForYouTubeURL:youTubeURL success:^(NSURL *URL) {
+		NSLog(@"Finished extracting: %@", URL);
+		MPMoviePlayerViewController *moviePlayer = [[MPMoviePlayerViewController alloc] initWithContentURL:URL];
+		[self presentModalViewController:moviePlayer animated:YES];
+	} failure:^(NSError *error) {
+		NSLog(@"Failed to query mp4: %@", error);
+	}];
+
+}
 @end
